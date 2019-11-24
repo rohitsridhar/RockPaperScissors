e00c9c3095ab63ac6c710a7f6404d2327acd118b
Rohit Sridhar <sridh072@umn.edu>
2019-11-24 01:03:00 -0800

    Slight improvements and experimenting with dict

--- game/rock_paper_scissors.py
+++ game/rock_paper_scissors.py
@@ -1,36 +1,46 @@
 from random import randint
+import time
 
 # create a list of play options
-t = ["Rock", "Paper", "Scissors"]
+t = {"1": "Rock", "2": "Paper", "3": "Scissors"}
 
 # assign a random play to the computer
-computer = t[randint(0, 2)]
+computer = randint(1, 3)
 
 # set player to False
 player = False
 
 while not player:
     # set player to True
-    player = input("Rock, Paper, Scissors?")
-    if player == computer:
+    print("Choose 1 of the following:")
+    print("1.Rock \n2.Paper \n3.Scissors \n4.Quit")
+    player = input()
+    if player == "4":
+        print("Thanks for playing!")
+        break
+    print("You chose:", t.get(player))
+    print("Computer chose:", t.get(str(computer)))
+    time.sleep(1)
+    if player == str(computer):
         print("Tie!")
-    elif player == "Rock":
-        if computer == "Paper":
-            print("You lose!", computer, "covers", player)
+    elif player == "1":
+        if computer == 2:
+            print("You lose!", t.get(str(computer)), "covers", t.get(player) + ".")
         else:
-            print("You win!", player, "smashes", computer)
-    elif player == "Paper":
-        if computer == "Scissors":
-            print("You lose!", computer, "cut", player)
+            print("You win!", t.get(player), "smashes", t.get(str(computer))+".")
+    elif player == "2":
+        if computer == 3:
+            print("You lose!", t.get(str(computer)), "cuts", t.get(player)+".")
         else:
-            print("You win!", player, "covers", computer)
-    elif player == "Scissors":
-        if computer == "Rock":
-            print("You lose...", computer, "smashes", player)
+            print("You win!", t.get(player), "covers", t.get(str(computer))+".")
+    elif player == "3":
+        if computer == 1:
+            print("You lose!", t.get(str(computer)), "smashes", t.get(player)+".")
         else:
-            print("You win!", player, "cut", computer)
+            print("You win!", t.get(player), "cut", t.get(str(computer))+".")
     else:
-        print("That's not a valid play. Check your spelling!")
+        print("Invalid choice. Choose again.")
+    print()
     # player was set to True, but we want it to be False so the loop continues
     player = False
-    computer = t[randint(0, 2)]
+    computer = randint(1, 3)
